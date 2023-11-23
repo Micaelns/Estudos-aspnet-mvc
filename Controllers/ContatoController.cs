96ad34b038fd684227fd6b167ffc2e8e10a70617
Micael Souza <micael.ns@hotmail.com>
2023-11-23 10:13:56 -0300

    exemplo funcional de AspNetMVC

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if ( ModelState.IsValid ) {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if ( contato == null ) {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            if ( ModelState.IsValid ) {
+                _context.Contatos.Update(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if ( contato == null ) {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if ( contato == null ) {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBc = _context.Contatos.Find(contato.Id);
+            if ( contatoBc != null ) {
+                _context.Contatos.Remove(contatoBc);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
